f7a6950962c1f96aa395577a9aeb16a8bc711fb5
Abhi Barkade <contact.abhibarkade@gmail.com>
2021-12-10 15:14:06 +0530

    First Application on GitHub

---
+++ app/src/main/java/com/abhi/videoplayer/MainActivity.java
@@ -0,0 +1,33 @@
+package com.abhi.videoplayer;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        videoView = findViewById(R.id.video);
+
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.sample);
+
+        MediaController controller = new MediaController(this);
+        controller.setAnchorView(videoView);
+
+        videoView.setMediaController(controller);
+
+        videoView.start();
+    }
+}
